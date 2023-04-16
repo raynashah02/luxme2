27a1c054625cd14faf5ba1f965b15931474890ed
raynashah02 <108150607+raynashah02@users.noreply.github.com>
2023-04-16 16:24:11 -0500

    Initial Commit

---
+++ luxme2/luxme2App.swift
@@ -0,0 +1,17 @@
+//
+//  luxme2App.swift
+//  luxme2
+//
+//  Created by Rayna Shah on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct luxme2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
